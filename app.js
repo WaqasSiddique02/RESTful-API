cf35d07a52d2d6fe7fcca755209906d722eb8ed7
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-07-18 14:56:47 +0500

    SettingUp the server

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const mongoose=require('mongoose');
+
+const app=express();
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+const articleSchema=new mongoose.Schema({
+    title:String,
+    content:String
+});
+const Article=mongoose.model("Article",articleSchema);
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+});
